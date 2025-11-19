7710e360e6efb7b2831124044c82658e6fd9bf52
rohnish-2006 <rohnish.2006@gmail.com>
2025-11-20 01:22:59 +0530

    Add ReverseString.java with Scanner input

---
+++ Elementary_Coding_Sheet_02/ReverseString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static String reverseString(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        String result = reverseString(input);
+        System.out.println("Reversed: " + result);
+        sc.close();
+    }
+}
