c5fbd47f9d7efb083ba11503a6fdf541a17f9bea
rohnish-2006 <rohnish.2006@gmail.com>
2025-11-20 01:01:21 +0530

    Add PrimesInRange.java with Scanner input
    This Java program counts the number of prime numbers within a specified range provided by the user.

---
+++ Elementary_Coding_Sheet_01/PrimesInRange.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PrimesInRange {
+    
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+    
+    public static int countPrimesInRange(int start, int end) {
+        int count = 0;
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter start of range: ");
+        int start = sc.nextInt();
+        System.out.print("Enter end of range: ");
+        int end = sc.nextInt();
+        int result = countPrimesInRange(start, end);
+        System.out.println("Number of primes in range: " + result);
+        sc.close();
+    }
+}
