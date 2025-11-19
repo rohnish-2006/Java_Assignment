b0d42cc676a881efc2e95713cc253e07dde3e0b6
rohnish-2006 <rohnish.2006@gmail.com>
2025-11-20 01:22:34 +0530

    Add CountVowels.java with Scanner input

---
+++ Elementary_Coding_Sheet_02/CountVowels.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CountVowels {
+    public static int countVowels(String str) {
+        int count = 0;
+        String vowels = "aeiouAEIOU";
+        for (char c : str.toCharArray()) {
+            if (vowels.indexOf(c) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        int count = countVowels(input);
+        System.out.println("Number of vowels: " + count);
+        sc.close();
+    }
+}
