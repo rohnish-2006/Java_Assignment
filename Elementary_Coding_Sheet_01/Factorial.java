666794c7042abab03b20535361db0a124adb957d
rohnish-2006 <rohnish.2006@gmail.com>
2025-11-20 00:33:56 +0530

    Add Factorial.java solution

---
+++ Elementary_Coding_Sheet_01/Factorial.java
@@ -0,0 +1,18 @@
+public class Factorial {
+    public static long factorial(int n) {
+        if (n < 0) return -1;
+        if (n == 0 || n == 1) return 1;
+        
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(factorial(5));  // 120
+        System.out.println(factorial(0));  // 1
+        System.out.println(factorial(10)); // 3628800
+    }
+}
